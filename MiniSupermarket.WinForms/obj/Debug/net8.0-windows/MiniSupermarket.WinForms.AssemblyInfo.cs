5f2ce9bccb87ea9f5e0c940ebc10c81d41f42928
NguyenDucLa <nguyenducla113@gmail.com>
2026-09-19 17:58:54 +0700

    Add README.md

--- MiniSupermarket.WinForms/obj/Debug/net8.0-windows/MiniSupermarket.WinForms.AssemblyInfo.cs
+++ MiniSupermarket.WinForms/obj/Debug/net8.0-windows/MiniSupermarket.WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniSupermarket.WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27188d384696d5b3d7032e15306f3916ed141e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniSupermarket.WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniSupermarket.WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
